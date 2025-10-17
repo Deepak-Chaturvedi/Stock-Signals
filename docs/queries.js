add47b35f884849d7a9bacf07b51491dba5ed911
Deepak Chaturvedi <deepak.chaturvedi.in@gmail.com>
2025-10-18 03:03:01 +0530

    Update queries.js

--- docs/queries.js
+++ docs/queries.js
@@ -17,17 +17,18 @@ SELECT DISTINCT
   DATE(A.Signal_date) AS 'Signal Date',
   CAST(ROUND(a.Signal_Price,0) AS INTEGER) AS 'Signal Price',
   CAST(ROUND(current_price, 0) AS INTEGER) AS 'Current Price',
-  cast(round(ret_1w_perc  ,0)as INTEGER)   AS '1 Week Return %',
-  cast(round(ret_2w_perc  ,0)as INTEGER)   AS '2 Week Return %',
-  cast(round(ret_1m_perc  ,0)as INTEGER)   AS '1 Month Return %',
-  cast(round(ret_3m_perc  ,0)as INTEGER)   AS '3 Month Return %',
-  cast(round(ret_6m_perc  ,0)as INTEGER)   AS '6 Month Return %',
-  cast(round(ret_1y_perc  ,0)as INTEGER)   AS '1 Year Return %',
-  cast(round(ret_sinceSignal_perc ,0) AS INTEGER)  AS 'Return Since Signal %'
+  cast(round(ret_1w_perc  ,0)as INTEGER) || '%' AS '1 Week Return %',
+  cast(round(ret_2w_perc  ,0)as INTEGER) || '%' AS '2 Week Return %',
+  cast(round(ret_1m_perc  ,0)as INTEGER) || '%' AS '1 Month Return %',
+  cast(round(ret_3m_perc  ,0)as INTEGER) || '%' AS '3 Month Return %',
+  cast(round(ret_6m_perc  ,0)as INTEGER) || '%' AS '6 Month Return %',
+  cast(round(ret_1y_perc  ,0)as INTEGER) || '%' AS '1 Year Return %',
+  cast(round(ret_sinceSignal_perc ,0) AS INTEGER) || '%' AS 'Return Since Signal %'
   FROM SIGNAL_RETURNS AS A
   LEFT JOIN COMPANY_DETAILS AS B
     ON A.Symbol = B.Symbol
   WHERE B.EXCHANGE != 'BSE'
-  ORDER BY  7 DESC,8 DESC,9 DESC,10 DESC,11 DESC,12 DESC,13 DESC;  
+  ORDER BY  7 DESC,8 DESC,9 DESC,10 DESC,11 DESC,12 DESC,13 DESC;
+
 `;
 // script.js
